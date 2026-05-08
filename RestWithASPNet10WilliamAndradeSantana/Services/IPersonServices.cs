32161590b7b41491365f4603e1d0a8fe1f0f3c9b
William Santana <williamandrade1058@gmail.com>
2026-05-08 18:42:05 -0300

    Definindo a Interface IPersonService

---
+++ RestWithASPNet10WilliamAndradeSantana/Services/IPersonServices.cs
@@ -0,0 +1,12 @@
+﻿using RestWithASPNet10WilliamAndradeSantana.Model;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Services;
+
+public interface IPersonServices
+{
+    Person CreatePerson(Person person);
+    Person FindById(long id);
+    List<Person> FindAll();
+    Person UpdatePerson(Person person);
+    void DeletePerson(long id);
+}
