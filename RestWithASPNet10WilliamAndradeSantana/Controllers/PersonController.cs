16a8ec322b478e76b8f7ec79b1c20aefcdbd7790
William Santana <williamandrade1058@gmail.com>
2026-05-08 18:34:00 -0300

    Implementando as Classes Controller e Model

---
+++ RestWithASPNet10WilliamAndradeSantana/Controllers/PersonController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PersonController : ControllerBase
+{
+
+}
