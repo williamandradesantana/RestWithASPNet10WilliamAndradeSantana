10db687def26e500aba6c7172f1d2fb90af2045d
William Santana <williamandrade1058@gmail.com>
2026-05-11 08:30:02 -0300

    Mapeando a Entidade com Entitity Manager e SQL Server

--- RestWithASPNet10WilliamAndradeSantana/Model/Person.cs
+++ RestWithASPNet10WilliamAndradeSantana/Model/Person.cs
@@ -1,11 +1,34 @@
-﻿namespace RestWithASPNet10WilliamAndradeSantana.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Model
 {
+    [Table("person")]
     public class Person
     {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
+
+        [Required]
+        [Column("first_name", TypeName = "varchar(80)")]
+        [MaxLength(80)]
         public string FirstName { get; set; }
+        
+        [Required]
+        [Column("last_name", TypeName = "varchar(80)")]
+        [MaxLength(80)]
         public string LastName { get; set; }
+
+        [Required]
+        [Column("address", TypeName = "varchar(100)")]
+        [MaxLength(100)]
         public string Address { get; set; }
+
+        [Required]
+        [Column("gender", TypeName = "varchar(6)")]
+        [MaxLength(6)]
         public string Gender { get; set; }
     }
 }
