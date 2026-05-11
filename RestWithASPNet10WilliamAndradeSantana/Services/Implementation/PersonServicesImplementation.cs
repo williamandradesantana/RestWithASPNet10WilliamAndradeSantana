0b080dd18a7e4524112dbb992ef1cab469482dac
William Santana <williamandrade1058@gmail.com>
2026-05-11 09:12:51 -0300

    Removendo os Mocks das outras Operações do CRUD com SQL Server

--- RestWithASPNet10WilliamAndradeSantana/Services/Implementation/PersonServicesImplementation.cs
+++ RestWithASPNet10WilliamAndradeSantana/Services/Implementation/PersonServicesImplementation.cs
@@ -5,7 +5,7 @@ namespace RestWithASPNet10WilliamAndradeSantana.Services.Implementation;
 
 public class PersonServicesImplementation : IPersonServices
 {
-    private MSSQLContext _context;
+    private readonly MSSQLContext _context;
 
     public PersonServicesImplementation(MSSQLContext context)
     {
@@ -14,44 +14,34 @@ public class PersonServicesImplementation : IPersonServices
 
     public List<Person> FindAll()
     {
-        return _context.Persons.OrderByDescending((person) => person.FirstName).ToList(); ;
+        return _context.Persons.OrderByDescending((person) => person.FirstName).ToList();
     }
 
     public Person FindById(long id)
     {
         var person = _context.Persons.FirstOrDefault((p) => p.Id == id);
-        if (person == null)
-        {
-            throw new KeyNotFoundException("Person not found!");
-        }
-        return person;
+        return (person == null) ? throw new KeyNotFoundException($"Person with id {id} not found") : person;
     }
 
     public Person CreatePerson(Person person)
     {
-        person.Id = Random.Shared.Next(1, 10000);
+        _context.Persons.Add(person);
+        _context.SaveChanges();
         return person;
     }
 
     public Person UpdatePerson(Person person)
     {
-        return person;
+        var personFound = FindById(person.Id);   
+        _context.Entry(personFound).CurrentValues.SetValues(person);
+        _context.SaveChanges();
+        return personFound;
     }
 
     public void DeletePerson(long id)
     {
-        // Simulate deletion logic
-    }
-
-    private Person MockPerson(int i)
-    {
-        return new Person
-        {
-            Id = Random.Shared.Next(1, 10000),
-            FirstName = "William " + i,
-            LastName = "Andrade " + i,
-            Address = "Aracaju - SE - Brazil " + i,
-            Gender = (i % 2 == 0) ? "Male" : "Female"
-        };
+        var person = FindById(id);
+        _context.Persons.Remove(person);
+        _context.SaveChanges();
     }
 }
