91684adfac96f2cf514f14c1166307f0a7036037
William Santana <williamandrade1058@gmail.com>
2026-05-13 13:51:26 -0300

    Mudando o Nível de Log Globalmente usando Serilog no ASP.NET 10

---
+++ RestWithASPNet10WilliamAndradeSantana/Controllers/TestLogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestLogController : ControllerBase
+{
+    private readonly ILogger<TestLogController> _logger;
+    public TestLogController(ILogger<TestLogController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult LogTest()
+    {
+        _logger.LogTrace("This is a TRACE log message");
+        _logger.LogDebug("This is a DEBUG log message");
+        _logger.LogInformation("This is an INFORMATION log message");
+        _logger.LogWarning("This is a WARNING log message");
+        _logger.LogError("This is an ERROR log message");
+        _logger.LogCritical("This is a CRITICAL log message");
+        return Ok("Log messages have been generated. Click your logging checkout");
+    }
+
+}
