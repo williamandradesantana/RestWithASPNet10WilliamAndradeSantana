8a10642bb6581b5482fdb1dab2b67d2b0a48cd11
William Santana <williamandrade1058@gmail.com>
2026-05-15 17:39:52 -0300

    Separando a Lógica da Aplicação em Camadas

--- RestWithASPNet10WilliamAndradeSantana/Services/Implementation/PersonServicesImplementation.cs
+++ RestWithASPNet10WilliamAndradeSantana/Services/Implementation/PersonServicesImplementation.cs
@@ -1,48 +1,39 @@
 ﻿using RestWithASPNet10WilliamAndradeSantana.Model;
-using RestWithASPNet10WilliamAndradeSantana.Model.Context;
+using RestWithASPNet10WilliamAndradeSantana.Repositories;
 
 namespace RestWithASPNet10WilliamAndradeSantana.Services.Implementation;
 
 public class PersonServicesImplementation : IPersonServices
 {
-    private readonly MSSQLContext _context;
+    private IPersonRepository _repository;
 
-    public PersonServicesImplementation(MSSQLContext context)
+    public PersonServicesImplementation(IPersonRepository repository)
     {
-        _context = context;
+        _repository = repository;
     }
 
     public List<Person> FindAll()
     {
-        return _context.Persons.ToList();
+        return _repository.FindAll();
     }
 
     public Person FindById(long id)
     {
-        return _context.Persons.Find(id);
+        return _repository.FindById(id);
     }
 
     public Person Create(Person person)
     {
-        _context.Add(person);
-        _context.SaveChanges();
-        return person;
+        return _repository.Create(person);
     }
 
     public Person Update(Person person)
     {
-        var existingPerson = _context.Persons.Find(person.Id);
-        if (existingPerson == null) return null;
-
-        _context.Entry(existingPerson).CurrentValues.SetValues(person);
-        _context.SaveChanges();
-        return person;
+        return _repository.Update(person);
     }
+
     public void Delete(long id)
     {
-        var existingPerson = _context.Persons.Find(id);
-        if (existingPerson == null) return;
-        _context.Remove(existingPerson);
-        _context.SaveChanges();
+        _repository.Delete(id);
     }
 }
