76fced8d3878bf893e0f2586ad43abcf55b90fb3
William Santana <williamandrade1058@gmail.com>
2026-05-16 23:35:08 -0300

    Executando as Primeiras Migrations com Evolve e Entity Framework

---
+++ RestWithASPNet10WilliamAndradeSantana/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender])
+VALUES (N'John', N'Doe', N'123 Main St', N'Male');
+
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender])
+VALUES (N'Jane', N'Doe', N'456 Oak Ave', N'Female');
+
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender])
+VALUES (N'Bob', N'Smith', N'789 Pine Rd', N'Male');
