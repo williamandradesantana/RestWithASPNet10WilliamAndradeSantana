77d03bf75604a8dc33b09d5a9a55e48d33264fd4
William Santana <williamandrade1058@gmail.com>
2026-05-24 09:24:58 -0300

    Ajustando o Converter e o Service da API Versionada em ASP.NET 10

---
+++ RestWithASPNet10WilliamAndradeSantana/Services/IPersonServicesV2.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNet10WilliamAndradeSantana.Data.DTO.V2;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Services;
+
+public interface IPersonServicesV2
+{
+    PersonDTO Create(PersonDTO person);
+    PersonDTO FindById(long id);
+    List<PersonDTO> FindAll();
+    PersonDTO Update(PersonDTO person);
+    void Delete(long id);
+    bool ExistsPerson(long id);
+}
---
+++ RestWithASPNet10WilliamAndradeSantana/Services/Implementation/PersonServicesImplementationV2.cs
@@ -0,0 +1,52 @@
+﻿using RestWithASPNet10WilliamAndradeSantana.Data.Converter.Implementation;
+using RestWithASPNet10WilliamAndradeSantana.Data.DTO.V2;
+using RestWithASPNet10WilliamAndradeSantana.Model;
+using RestWithASPNet10WilliamAndradeSantana.Repositories;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Services.Implementation;
+
+public class PersonServicesImplementationV2 : IPersonServicesV2
+{
+    private IRepository<Person> _repository;
+    private readonly PersonConverterV2 _converter;
+
+    public PersonServicesImplementationV2(IRepository<Person> repository)
+    {
+        _repository = repository;
+        _converter = new PersonConverterV2();
+    }
+
+    public List<PersonDTO> FindAll()
+    {
+        return _converter.ParseList(_repository.GetAll()).ToList();
+    }
+
+    public PersonDTO FindById(long id)
+    {
+        return _converter.Parse(_repository.FindById(id));
+    }
+
+    public PersonDTO Create(PersonDTO person)
+    {
+        var entity = _converter.Parse(person);
+        entity = _repository.Create(entity);
+        return _converter.Parse(entity);
+    }
+
+    public PersonDTO Update(PersonDTO person)
+    {
+        var entity = _converter.Parse(person);
+        entity = _repository.Update(entity);
+        return _converter.Parse(entity);
+    }
+
+    public void Delete(long id)
+    {
+        _repository.Delete(id);
+    }
+
+    public bool ExistsPerson(long id)
+    {
+        return _repository.Exists(id);
+    }
+}
