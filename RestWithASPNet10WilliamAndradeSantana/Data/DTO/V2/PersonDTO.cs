16dde3a68b66f0ec6991d3cb3b393b0dcbcf9274
William Santana <williamandrade1058@gmail.com>
2026-05-28 12:46:41 -0300

    Custom JSON Serialization com JsonPropertyName e JsonPropertyOrder

--- RestWithASPNet10WilliamAndradeSantana/Data/DTO/V2/PersonDTO.cs
+++ RestWithASPNet10WilliamAndradeSantana/Data/DTO/V2/PersonDTO.cs
@@ -1,10 +1,19 @@
-﻿namespace RestWithASPNet10WilliamAndradeSantana.Data.DTO.V2;
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Data.DTO.V2;
 
 public class PersonDTO
 {
+    [JsonPropertyName("code")]
     public long Id { get; set; }
+
+    [JsonPropertyName("first_name")]
     public string FirstName { get; set; }
+
+    [JsonPropertyName("last_name")]
     public string LastName { get; set; }
+
+    //[JsonPropertyOrder(1)]
     public string Address { get; set; }
     public string Gender { get; set; }
     public DateTime? BirthDay { get; set; }
