bdf24a1a2668fd2a0c748e3af942b40c872d891f
William Santana <williamandrade1058@gmail.com>
2026-09-08 15:05:08 -0300

    Criando a WebApplicationFactory para Possibilitar Testes de Integração no ASP.NET 10

---
+++ RestWithASPNet10WilliamAndradeSantana.Tests/IntegrationTests/Tools/CustomWebApplicationFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Tests.IntegrationTests.Tools;
+
+public class CustomWebApplicationFactory<TProgram> 
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    private readonly string _connectionString;
+
+    public CustomWebApplicationFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            var dict = new Dictionary<string, string>
+            {
+                {
+                    "MSSQLServerSQLConnection:MSSQLServerSQLConnectionString", _connectionString
+                }
+            };
+            config.AddInMemoryCollection(dict!);
+        });
+    }
+}
