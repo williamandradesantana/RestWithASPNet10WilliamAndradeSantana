6b189a400bf06b79bbc1ae8199c2b5df0a7523ae
William Santana <williamandrade1058@gmail.com>
2026-09-14 09:14:20 -0300

    Implementando a Classe Utilitária PriorityOrderer

---
+++ RestWithASPNet10WilliamAndradeSantana.Tests/IntegrationTests/Tools/PriorityOrder.cs
@@ -0,0 +1,29 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace RestWithASPNet10WilliamAndradeSantana.Tests.IntegrationTests.Tools;
+
+public class PriorityOrder : ITestCaseOrderer
+{
+    public IEnumerable<TTestCase> OrderTestCases<TTestCase>
+        (IEnumerable<TTestCase> testCases) 
+        where TTestCase : ITestCase
+    {
+        var sortedMethods = testCases.OrderBy(
+            tc => tc.TestMethod.Method
+            .GetCustomAttributes(typeof(TestPriorityAttribute))
+            .FirstOrDefault()
+            ?.GetNamedArgument<int>("Priority") ?? 0);
+        return sortedMethods;
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestPriorityAttribute : Attribute
+    {
+        public int Priority { get; }
+
+        public TestPriorityAttribute(int priority)
+        => Priority = priority;
+    }
+}
+
